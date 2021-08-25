5d27dc195fd9706689370cb6fb3f29c9a02399be
Bato007 <brandonhernandez_10@outlook.es>
2021-08-25 10:46:25 -0600

    decrypt

--- cipher.py
+++ cipher.py
@@ -1,8 +1,8 @@
-from Crypto.Cipher import AES
-from base64 import b64encode
-from Crypto.Util.Padding import pad
-from Crypto.Random import get_random_bytes
+from base64 import b64decode, b64encode
 
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import pad, unpad
 
 '''
 MODO CBC
@@ -13,7 +13,6 @@ MODO CBC
 def AEScipher(key16, txtToCipher, txtToReturn):
     cipher = AES.new(key16, AES.MODE_CBC)
     iv = b64encode(cipher.iv).decode('utf-8')
-
     cipherTxt = open(txtToReturn,"w+")
     
     with open(txtToCipher) as a:
@@ -26,8 +25,32 @@ def AEScipher(key16, txtToCipher, txtToReturn):
     a.close()
     cipherTxt.close()
 
-    return str(iv)
+    return str(iv), cipher.iv
 
-key = get_random_bytes(16)
+def AESdecrypt(key16, cipherTxt, returnTxt, iv):
+    cipher = AES.new(key16, AES.MODE_CBC, iv)
+    decryptedTxt = open(returnTxt, 'w+')
 
-print('IV ', AEScipher(key, 'prueba.txt', 'prueba.enc'))
+    # Ahora se obtiene el texto
+    with open(cipherTxt) as ctxt:
+        line = ctxt.read()
+        ct = b64decode(line)
+        bt = unpad(cipher.decrypt(ct), AES.block_size)
+        pt = bt.decode()
+        decryptedTxt.write(pt)
+
+    # Escribiendo en el txt y 
+    ctxt.close()
+    decryptedTxt.close()
+
+def main():
+    txt = 'prueba.txt'
+    enctxt = 'prueba.enc'
+    dectxt = 'prueba.dec'
+    key = get_random_bytes(16)
+
+    striv, iv =  AEScipher(key, txt, enctxt)
+    AESdecrypt(key, enctxt, dectxt, iv)
+
+if __name__ == '__main__':
+    main()
